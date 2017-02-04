e16fec66ac92e7bd51f2e797157ad5886b1e3158
ashrit <ashrits@g.clemson.edu>
2017-02-03 22:21:38 -0500

    Bug Fixes

--- matrix-multiplication.c
+++ matrix-multiplication.c
@@ -2,180 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
-void read_imatrix(char *filename, int *m, int *n, int **values)
-{
-	FILE* name;
-	int i, j, k;
-	int t1, t2, t3;
-	name = fopen(filename, "r+");
-	if(name != NULL)
-	{
-		k = 0;
-	  fscanf(name, "%d %d\n", &t1, &t2);
-		*m = t1;
-		*n = t2;
-		*values = (int *)calloc(t1*t2, sizeof(int));
-	  for(i = 1; i <= t1; i++)
-	  {
-	    for(j = 1; j <= t2; j++)
-	    {
-				if(j < t2)
-				{
-		  		fscanf(name, "%d,", &t3);
-					*(*values+k) = t3;
-		  		k++;
-				}
-				else
-				{
-		  		fscanf(name, "%d\n", &t3);
-					*(*values+k) = t3;
-		  		k++;
-				}
-			}
-		}
-	  fclose(name);
-	}
-	else
-	{
-	  printf("File read failed\n");
-	}
-}
-
-void read_fmatrix(char *filename, int *m, int *n, float **values)
-{
-	FILE* name;
-	int i, j, k;
-	int t1, t2;
-	float t3;
-	name = fopen(filename, "r+");
-	if(name != NULL)
-	{
-		k = 0;
-	  fscanf(name, "%d %d\n", &t1, &t2);
-		*m = t1;
-		*n = t2;
-		*values = (float *)calloc(t1*t2, sizeof(float));
-  	for(i = 1; i <= t1; i++)
-    {
-    	for(j = 1; j <= t2; j++)
-      {
-				if(j < t2)
-				{
-	  			fscanf(name, "%f,", &t3);
-					*(*values+k) = t3;
-	  			k++;
-				}
-				else
-				{
-	  			fscanf(name, "%f\n", &t3);
-					*(*values+k) = t3;
-	  			k++;
-				}
-			}
-		}
-    fclose(name);
-  }
-  else
-  {
-    printf("File read failed\n");
-  }
-}
-
-void write_imatrix(char *filename, int *m, int *n, int **values)
-{
-	FILE* name;
-	int i, j, k;
-	int t1, t2, t3;
-	name = fopen(filename, "w+");
-	if(name != NULL)
-	{
-		k = 0;
-		t1 = *m;
-		t2 = *n;
-	  fprintf(name, "%d %d\n", t1, t2);
-    for(i = 1; i <= t1; i++)
-    {
-      for(j = 1; j <= t2; j++)
-      {
-				if(j < t2)
-				{
-					t3 = *(*values+k);
-		  		fprintf(name, "%d,", t3);
-		  		k++;
-				}
-				else
-				{
-					t3 = *(*values+k);
-		  		fprintf(name, "%d\n", t3);
-		  		k++;
-				}
-			}
-    }
-    fclose(name);
-  }
-  else
-  {
-    printf("File write failed\n");
-  }
-}
-
-void write_fmatrix(char *filename, int *m, int *n, float **values)
-{
-	FILE* name;
-	int i, j, k;
-	int t1, t2;
-	float t3;
-	name = fopen(filename, "w+");
-	if(name != NULL)
-	{
-		k = 0;
-		t1 = *m;
-		t2 = *n;
-	  fprintf(name, "%d %d\n", t1, t2);
-    for(i = 1; i <= t1; i++)
-    {
-      for(j = 1; j <= t2; j++)
-      {
-				if(j < t2)
-				{
-					t3 = *(*values+k);
-		  		fprintf(name, "%f,", t3);
-		  		k++;
-				}
-				else
-				{
-					t3 = *(*values+k);
-		  		fprintf(name, "%f\n", t3);
-		  		k++;
-				}
-			}
-    }
-    fclose(name);
-  }
-  else
-  {
-    printf("File write failed\n");
-  }
-}
+#include <time.h>
 
 void multiply_imatrix(int **input1, int **input2, int **output, int *m, int *n, int *p, int *q)
 {
-	int i, j, k;
-	int offset1, offset2, offset3;
-	int sum = 0;
-	for (i = 0; i < *m; i++)
-	{
+  int i, j, k;
+  int offset1, offset2, offset3;
+  int sum = 0;
+  for (i = 0; i < *m; i++)
+  {
     for (j = 0; j < *q; j++)
-		{
+    {
       for (k = 0; k < *p; k++)
-			{
-				offset1 = i*(*n) + k;
-				offset2 = k*(*q) + j;
+      {
+        offset1 = i*(*n) + k;
+        offset2 = k*(*q) + j;
         sum = sum + *(*input1+offset1) * *(*input2+offset2);
       }
-			offset3 = i*(*q) + j;
- 			*(*output+offset3) = sum;
+      offset3 = i*(*q) + j;
+      *(*output+offset3) = sum;
       sum = 0.0;
     }
   }
@@ -183,23 +28,183 @@ void multiply_imatrix(int **input1, int **input2, int **output, int *m, int *n, 
 
 void multiply_fmatrix(float **input1, float **input2, float **output, int *m, int *n, int *p, int *q)
 {
-	int i, j, k;
-	int offset1, offset2, offset3;
-	float sum = 0.0;
-	for (i = 0; i < *m; i++)
-	{
+  int i, j, k;
+  int offset1, offset2, offset3;
+  float sum = 0.0;
+  for (i = 0; i < *m; i++)
+  {
     for (j = 0; j < *q; j++)
-		{
+    {
       for (k = 0; k < *p; k++)
-			{
-				offset1 = i*(*n) + k;
-				offset2 = k*(*q) + j;
+      {
+        offset1 = i*(*n) + k;
+        offset2 = k*(*q) + j;
         sum = sum + *(*input1+offset1) * *(*input2+offset2);
       }
-			offset3 = i*(*q) + j;
- 			*(*output+offset3) = sum;
+      offset3 = i*(*q) + j;
+      *(*output+offset3) = sum;
       sum = 0.0;
     }
+  }
+}
+
+void read_imatrix(char *filename, int *m, int *n, int **values)
+{
+  FILE* name;
+  int i, j, k;
+  int t1, t2, t3;
+  name = fopen(filename, "r+");
+  if(name != NULL)
+  {
+    k = 0;
+    fscanf(name, "%d %d\n", &t1, &t2);
+    *m = t1;
+    *n = t2;
+    *values = (int *)calloc(t1*t2, sizeof(int));
+    for(i = 1; i <= t1; i++)
+    {
+      for(j = 1; j <= t2; j++)
+      {
+        if(j < t2)
+        {
+          fscanf(name, "%d,", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+        else
+        {
+          fscanf(name, "%d\n", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+      }
+    }
+    fclose(name);
+  }
+  else
+  {
+    printf("File read failed\n");
+    exit(1);
+  }
+}
+
+void read_fmatrix(char *filename, int *m, int *n, float **values)
+{
+  FILE* name;
+  int i, j, k;
+  int t1, t2;
+  float t3;
+  name = fopen(filename, "r+");
+  if(name != NULL)
+  {
+    k = 0;
+    fscanf(name, "%d %d\n", &t1, &t2);
+    *m = t1;
+    *n = t2;
+    *values = (float *)calloc(t1*t2, sizeof(float));
+    for(i = 1; i <= t1; i++)
+    {
+      for(j = 1; j <= t2; j++)
+      {
+        if(j < t2)
+        {
+          fscanf(name, "%f,", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+        else
+        {
+          fscanf(name, "%f\n", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+      }
+    }
+    fclose(name);
+  }
+  else
+  {
+    printf("File read failed\n");
+    exit(1);
+  }
+}
+
+void write_imatrix(char *filename, int *m, int *n, int **values)
+{
+  FILE* name;
+  int i, j, k;
+  int t1, t2, t3;
+  name = fopen(filename, "w+");
+  if(name != NULL)
+  {
+    k = 0;
+    t1 = *m;
+    t2 = *n;
+    fprintf(name, "%d %d\n", t1, t2);
+    for(i = 1; i <= t1; i++)
+    {
+      for(j = 1; j <= t2; j++)
+      {
+        if(j < t2)
+        {
+          t3 = *(*values+k);
+          fprintf(name, "%d,", t3);
+          k++;
+        }
+        else
+        {
+          t3 = *(*values+k);
+          fprintf(name, "%d\n", t3);
+          k++;
+        }
+      }
+    }
+    fclose(name);
+  }
+  else
+  {
+    printf("File write failed\n");
+    exit(1);
+  }
+}
+
+void write_fmatrix(char *filename, int *m, int *n, float **values)
+{
+  FILE* name;
+  int i, j, k;
+  int t1, t2;
+  float t3;
+  name = fopen(filename, "w+");
+  if(name != NULL)
+  {
+    k = 0;
+    t1 = *m;
+    t2 = *n;
+    fprintf(name, "%d %d\n", t1, t2);
+    for(i = 1; i <= t1; i++)
+    {
+      for(j = 1; j <= t2; j++)
+      {
+        if(j < t2)
+        {
+          t3 = *(*values+k);
+          fprintf(name, "%f,", t3);
+          k++;
+        }
+        else
+        {
+          t3 = *(*values+k);
+          fprintf(name, "%f\n", t3);
+          k++;
+        }
+      }
+    }
+    fclose(name);
+  }
+  else
+  {
+    printf("File write failed\n");
+    exit(1);
   }
 }
 
@@ -214,23 +219,27 @@ void matrix_check(int m1, int n1, int m2, int n2)
 
 int main(int argc, char *argv[])
 {
+  clock_t t1,t2,t3,t4;
+  t1 = clock();
   int m1, n1, m2, n2, matrix_size;
 
-	if (argc != 5)
-	{
-		printf("Usage: ./matrix-multiplication matrix1.mat matrix2.mat matrix3.mat float/int \n");
-		exit(1);
-	}
+  if (argc != 5)
+  {
+    printf("Usage: ./matrix-multiplication matrix1.mat matrix2.mat matrix3.mat float/int \n");
+    exit(1);
+  }
 
   if (strcmp(argv[4], "float") == 0)
   {
     float *hostmatrix1, *hostmatrix2, *hostmatrix3;
     read_fmatrix(argv[1], &m1, &n1, &hostmatrix1);
-  	read_fmatrix(argv[2], &m2, &n2, &hostmatrix2);
+    read_fmatrix(argv[2], &m2, &n2, &hostmatrix2);
     matrix_check(m1, n1, m2, n2);
     matrix_size = m1 * n1;
     hostmatrix3 = (float *)calloc(matrix_size, sizeof(float));
+    t2=clock();
     multiply_fmatrix(&hostmatrix1, &hostmatrix2, &hostmatrix3, &m1, &n1, &m2, &n2);
+    t3=clock();
     write_fmatrix(argv[3], &m1, &n1, &hostmatrix3);
     free(hostmatrix1);
     free(hostmatrix2);
@@ -241,16 +250,23 @@ int main(int argc, char *argv[])
   {
     int *hostmatrix1, *hostmatrix2, *hostmatrix3;
     read_imatrix(argv[1], &m1, &n1, &hostmatrix1);
-  	read_imatrix(argv[2], &m2, &n2, &hostmatrix2);
+    read_imatrix(argv[2], &m2, &n2, &hostmatrix2);
     matrix_check(m1, n1, m2, n2);
     matrix_size = m1 * n1;
     hostmatrix3 = (int *)calloc(matrix_size, sizeof(int));
+    t2=clock();
     multiply_imatrix(&hostmatrix1, &hostmatrix2, &hostmatrix3, &m1, &n1, &m2, &n2);
+    t3=clock();
     write_imatrix(argv[3], &m1, &n1, &hostmatrix3);
     free(hostmatrix1);
     free(hostmatrix2);
     free(hostmatrix3);
   }
 
+  t4=clock();
+  double time_k = (double)(t3-t2)/CLOCKS_PER_SEC;
+  double time_f = (double)(t4-t1)/CLOCKS_PER_SEC;
+  printf("Kernel time %f\n", time_k);
+  printf("Total time %f\n", time_f);
   return 0;
 }
--- matrix-multiplication.cu
+++ matrix-multiplication.cu
@@ -4,175 +4,222 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-#define BLOCKSIZE 3
+#define BLOCKSIZE 16
 
 __global__ void MatrixMultiplyI(int *matrix1, int *matrix2, int *matrix3, int m1, int n1, int m2, int n2)
 {
-	int i;
-	__shared__ float ds_M[BLOCKSIZE][BLOCKSIZE];
-	__shared__ float ds_N[BLOCKSIZE][BLOCKSIZE];
-	int bx = blockIdx.x;
-	int by = blockIdx.y;
-	int tx = threadIdx.x;
-	int ty = threadIdx.y;
-	int row = by * blockDim.y + ty;
-	int col = bx * blockDim.x + tx;
-	int sum = 0;
-	for (i = 0; i < n2/BLOCKSIZE; i++)
-	{
-		ds_M[ty][tx] = matrix1[row*n2 + i*BLOCKSIZE+tx];
-		ds_N[ty][tx] = matrix2[(i*BLOCKSIZE+ty)*n2 + col];
-		__syncthreads();
-		for (i = 0; i < BLOCKSIZE; i++)
-		{
-			sum = sum + (ds_M[ty][i] * ds_N[i][tx]);
-		}
-		__syncthreads();
-	}
-	matrix3[row*n2+col] = sum;
+  int i, j;
+
+  __shared__ int ds_M[BLOCKSIZE][BLOCKSIZE];
+  __shared__ int ds_N[BLOCKSIZE][BLOCKSIZE];
+
+  int by = blockIdx.y;
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+
+  int row = by * blockDim.y + ty;
+  int col = bx * blockDim.x + tx;
+
+  int sum = 0;
+  for (i = 0; i < (n2-1)/BLOCKSIZE+1; i++)
+  {
+    if(row < n2 && i*BLOCKSIZE+tx < n2)
+    {
+      ds_M[ty][tx] = matrix1[row*n2 + i*BLOCKSIZE+tx];
+    }
+    else
+    {
+      ds_M[ty][tx] = 0;
+    }
+    if(i*BLOCKSIZE+ty < n2 && col < n2)
+    {
+      ds_N[ty][tx] = matrix2[(i*BLOCKSIZE+ty)*n2 + col];
+    }
+    else
+    {
+      ds_N[ty][tx] = 0;
+    }
+    __syncthreads();
+    if(row < n2 && col < n2)
+    {
+      for (j = 0; j < BLOCKSIZE; j++)
+      {
+        sum = sum + (ds_M[ty][j] * ds_N[j][tx]);
+      }
+    }
+    __syncthreads();
+  }
+  if(row < n2 && col < n2)
+    matrix3[row*n2+col] = sum;
 }
 
 __global__ void MatrixMultiplyF(float *matrix1, float *matrix2, float *matrix3, int m1, int n1, int m2, int n2)
 {
-	int i;
-	__shared__ float ds_M[BLOCKSIZE][BLOCKSIZE];
-	__shared__ float ds_N[BLOCKSIZE][BLOCKSIZE];
-	int bx = blockIdx.x;
-	int by = blockIdx.y;
-	int tx = threadIdx.x;
-	int ty = threadIdx.y;
-	int row = by * blockDim.y + ty;
-	int col = bx * blockDim.x + tx;
-	float sum = 0.0;
-	for (i = 0; i < n2/BLOCKSIZE; i++)
-	{
-		ds_M[ty][tx] = matrix1[row*n2 + i*BLOCKSIZE+tx];
-		ds_N[ty][tx] = matrix2[(i*BLOCKSIZE+ty)*n2 + col];
-		__syncthreads();
-		for (i = 0; i < BLOCKSIZE; i++)
-		{
-			sum = sum + (ds_M[ty][i] * ds_N[i][tx]);
-		}
-		__syncthreads();
-	}
-	matrix3[row*n2+col] = sum;
+  int i, j;
+
+  __shared__ float ds_M[BLOCKSIZE][BLOCKSIZE];
+  __shared__ float ds_N[BLOCKSIZE][BLOCKSIZE];
+
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+
+  int row = by * blockDim.y + ty;
+  int col = bx * blockDim.x + tx;
+
+  float sum = 0.0;
+
+  for (i = 0; i < n2/BLOCKSIZE; i++)
+  {
+    if(row < n2 && i*BLOCKSIZE+tx < n2)
+    {
+      ds_M[ty][tx] = matrix1[row*n2 + i*BLOCKSIZE+tx];
+    }
+    else
+    {
+      ds_M[ty][tx] = 0.0;
+    }
+    if(i*BLOCKSIZE+ty < n2 && col < n2)
+    {
+      ds_N[ty][tx] = matrix2[(i*BLOCKSIZE+ty)*n2 + col];
+    }
+    else
+    {
+      ds_N[ty][tx] = 0.0;
+    }
+    __syncthreads();
+    if(row < n2 && col < n2)
+    {
+      for (j = 0; j < BLOCKSIZE; j++)
+      {
+        sum = sum + (ds_M[ty][j] * ds_N[j][tx]);
+      }
+    }
+    __syncthreads();
+  }
+  if(row < n2 && col < n2)
+    matrix3[row*n2+col] = sum;
 }
 
 void read_imatrix(char *filename, int *m, int *n, int **values)
 {
-	FILE* name;
-	int i, j, k;
-	int t1, t2, t3;
-	name = fopen(filename, "r+");
-	if(name != NULL)
-	{
-		k = 0;
-		fscanf(name, "%d %d\n", &t1, &t2);
-		*m = t1;
-		*n = t2;
-		*values = (int *)calloc(t1*t2, sizeof(int));
-		for(i = 1; i <= t1; i++)
-		{
-			for(j = 1; j <= t2; j++)
-			{
-				if(j < t2)
-				{
-					fscanf(name, "%d,", &t3);
-					*(*values+k) = t3;
-					k++;
-				}
-				else
-				{
-					fscanf(name, "%d\n", &t3);
-					*(*values+k) = t3;
-					k++;
-				}
-			}
-		}
-		fclose(name);
-	}
-	else
-	{
-		printf("File read failed\n");
-	}
+  FILE* name;
+  int i, j, k;
+  int t1, t2, t3;
+  name = fopen(filename, "r+");
+  if(name != NULL)
+  {
+    k = 0;
+    fscanf(name, "%d %d\n", &t1, &t2);
+    *m = t1;
+    *n = t2;
+    *values = (int *)calloc(t1*t2, sizeof(int));
+    for(i = 1; i <= t1; i++)
+    {
+      for(j = 1; j <= t2; j++)
+      {
+        if(j < t2)
+        {
+          fscanf(name, "%d,", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+        else
+        {
+          fscanf(name, "%d\n", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+      }
+    }
+    fclose(name);
+  }
+  else
+  {
+    printf("File read failed\n");
+    exit(1);
+  }
 }
 
 void read_fmatrix(char *filename, int *m, int *n, float **values)
 {
-	FILE* name;
-	int i, j, k;
-	int t1, t2;
-	float t3;
-	name = fopen(filename, "r+");
-	if(name != NULL)
-	{
-		k = 0;
-		fscanf(name, "%d %d\n", &t1, &t2);
-		*m = t1;
-		*n = t2;
-		*values = (float *)calloc(t1*t2, sizeof(float));
-		for(i = 1; i <= t1; i++)
-		{
-			for(j = 1; j <= t2; j++)
-			{
-				if(j < t2)
-				{
-					fscanf(name, "%f,", &t3);
-					*(*values+k) = t3;
-					k++;
-				}
-				else
-				{
-					fscanf(name, "%f\n", &t3);
-					*(*values+k) = t3;
-					k++;
-				}
-			}
-		}
-		fclose(name);
-	}
-	else
-	{
-		printf("File read failed\n");
-	}
+  FILE* name;
+  int i, j, k;
+  int t1, t2;
+  float t3;
+  name = fopen(filename, "r+");
+  if(name != NULL)
+  {
+    k = 0;
+    fscanf(name, "%d %d\n", &t1, &t2);
+    *m = t1;
+    *n = t2;
+    *values = (float *)calloc(t1*t2, sizeof(float));
+    for(i = 1; i <= t1; i++)
+    {
+      for(j = 1; j <= t2; j++)
+      {
+        if(j < t2)
+        {
+          fscanf(name, "%f,", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+        else
+        {
+          fscanf(name, "%f\n", &t3);
+          *(*values+k) = t3;
+          k++;
+        }
+      }
+    }
+    fclose(name);
+  }
+  else
+  {
+    printf("File read failed\n");
+    exit(1);
+  }
 }
 
 void write_imatrix(char *filename, int *m, int *n, int **values)
 {
-	FILE* name;
-	int i, j, k;
-	int t1, t2, t3;
-	name = fopen(filename, "w+");
-	if(name != NULL)
-	{
-		k = 0;
-		t1 = *m;
-		t2 = *n;
-		fprintf(name, "%d %d\n", t1, t2);
-		for(i = 1; i <= t1; i++)
-		{
-			for(j = 1; j <= t2; j++)
-			{
-				if(j < t2)
-				{
-					t3 = *(*values+k);
-					fprintf(name, "%d,", t3);
-					k++;
-				}
-				else
-				{
-					t3 = *(*values+k);
-					fprintf(name, "%d\n", t3);
-					k++;
-				}
-			}
-		}
-	fclose(name);
-	}
-	else
-	{
-	printf("File write failed\n");
-	}
+  FILE* name;
+  int i, j, k;
+  int t1, t2, t3;
+  name = fopen(filename, "w+");
+  if(name != NULL)
+  {
+    k = 0;
+    t1 = *m;
+    t2 = *n;
+    fprintf(name, "%d %d\n", t1, t2);
+    for(i = 1; i <= t1; i++)
+    {
+      for(j = 1; j <= t2; j++)
+      {
+        if(j < t2)
+        {
+          t3 = *(*values+k);
+          fprintf(name, "%d,", t3);
+          k++;
+        }
+        else
+        {
+          t3 = *(*values+k);
+          fprintf(name, "%d\n", t3);
+          k++;
+        }
+      }
+    }
+    fclose(name);
+  }
+  else
+  {
+    printf("File write failed\n");
+    exit(1);
+  }
 }
 
 void write_fmatrix(char *filename, int *m, int *n, float **values)
@@ -211,6 +258,7 @@ void write_fmatrix(char *filename, int *m, int *n, float **values)
   else
   {
     printf("File write failed\n");
+    exit(1);
   }
 }
 
@@ -290,5 +338,6 @@ int main(int argc, char *argv[])
     free(hostmatrix2);
     free(hostmatrix3);
   }
+
   return 0;
 }
